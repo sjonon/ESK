c60b23866c28dcd9d692b4575a22a3dd887e7508
sylvo <49357334+sjonon@users.noreply.github.com>
2019-09-22 22:04:14 -0500

    updated schedule page to inform visitors the festival is over for 2019, updated home page to list dates for 2020

--- client/src/pages/SchedulePic.js
+++ client/src/pages/SchedulePic.js
@@ -7,12 +7,18 @@ class SchedulePic extends Component {
 
     render() {
         return (
+            <div>
+            <ScheduleNav />
             <Container>
                 <div id="schedule">
                 <h1>Eastside Kings Festival</h1>
+                <p>The 2019 Eastside Kings Festival has wrapped up.  But while you wait for us to update the schedule for 2020, take a look at
+                    our schedule below for what took place in 2019 and enjoy listening to previews of some of the artists you loved from 2019 on our Schedule By Artist page.
+                </p>
                 <img src={esksched} alt="schedule"/>
                 </div>
             </Container>
+            </div>
         )
     }
 }
